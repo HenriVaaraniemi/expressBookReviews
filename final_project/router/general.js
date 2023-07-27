738d3efec27e4bf5bdb5bb9ee899e0d16c6c9c76
HenriVaaraniemi <henri.vaaraniemi@hotmail.fi>
2023-07-27 14:31:03 -0400

    saved

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,37 +7,51 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "Yet to be implemented1"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+    res.send(JSON.stringify(books,null,4));
+    
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
+  //return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const pituus = Object.keys(books).length;
+ // for (let i = 0; i<pituus; i++){
+ //   if (author == books.author[i]){
+ //       res.send(books[i]);
+ //   }
+ //   else{
+        
+  //  }
+ // };
+  
+  
+  res.send(books);
+  //res.send(author);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "Yet to be implemented3"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "Yet to be implemented4"});
 });
 
 module.exports.general = public_users;
